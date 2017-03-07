004cfb6abde9115b35040dc1fce6e6cad438d27a
maciej3031 <mpesko3031@gmail.com>
2017-03-07 16:27:53 +0000

    1. forced https added 2.run.py ignored from now

--- run.py
+++
@@ -1,6 +0,0 @@
-#!flask/bin/python
-# -*- coding: utf-8 -*-
-from todo import app
-
-if __name__ == '__main__':
-    app.run(debug=True)         # uruchomienie aplikacji
--- todo/__init__.py
+++ todo/__init__.py
@@ -3,11 +3,13 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from flask_sslify import SSLify
 
 app = Flask(__name__)               # stworzenie aplikacji
 app.config.from_object('config')       # konfiguracja aplikacji wczytana z modułu config.py
 db = SQLAlchemy(app)            # utworzenie obiektu bazy danych
 mail = Mail(app)            # utworzenie możliwości wysyłania emaili
+sslify = SSLify(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)         #połączenie flask-login z flask
